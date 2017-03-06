e1e3ce9a813304a5ea7406b9b9d6481a332cf6f5
wangqianli <qlw1214@163.com>
2017-03-06 16:00:07 +0800

    增加getCode方法

--- src/com/wql/util/HelloWord.java
+++ src/com/wql/util/HelloWord.java
@@ -2,8 +2,14 @@ package com.wql.util;
 
 public class HelloWord {
 	
+	
+	public static double getCode() {
+		return Math.random()*100;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello word!");
+		System.out.println(HelloWord.getCode());
 	}
 	
 }
